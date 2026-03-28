4dea56fe8b86bb8628a6ae394e82122c0b7ceb96
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-03-28 10:53:29 +0700

    chore: bump version to v1.1.0

--- src/main.c
+++ src/main.c
@@ -112,7 +112,7 @@ int main(int argc, char *argv[]) {
  
     if (argc > 1) {
         if (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0) {
-            printf("mandelbrot-c 1.0.0\n"); return 0;
+            printf("mandelbrot-c 1.1.0\n"); return 0;
         }
         if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
             print_controls(); return 0;
