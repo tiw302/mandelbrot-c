e474c812623274150a3ee24248f7e5cb3186f5a2
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-03-29 10:46:08 +0700

    renderer: clean up header comments

--- include/renderer.h
+++ include/renderer.h
@@ -1,5 +1,5 @@
-#ifndef RENDERER_H
-#define RENDERER_H
+#ifndef RENDER_H
+#define RENDER_H
 
 #include <SDL2/SDL.h>
 #include <stdatomic.h>
@@ -24,22 +24,22 @@ typedef struct {
     atomic_int      *next_row;
 } thread_data_t;
 
-/* Initialize global rendering resources (LUT) */
+// precompute LUT or other startup work
 void init_renderer(void);
 
-/* Maps fractional iteration to RGB using sine-wave gradients */
+// map iteration to RGB color
 void get_color(double iterations, Uint8 *r, Uint8 *g, Uint8 *b);
 
-/* Thread entry point for row-based work queue */
+// main worker thread function
 void *render_thread(void *arg);
 
-/* Threaded Mandelbrot rendering entry point */
+// multi-threaded mandelbrot renderer
 void render_mandelbrot_threaded(Uint32 *pixels, int pitch,
                                 int window_width, int window_height,
                                 double re_min, double re_max,
                                 double im_min, double im_max);
 
-/* Threaded Julia rendering entry point */
+// multi-threaded julia renderer
 void render_julia_threaded(Uint32 *pixels, int pitch,
                            int window_width, int window_height,
                            double re_min, double re_max,
