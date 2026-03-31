7822f3ff7250fb5f827ff6f6facc4b24353ef895
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-03-31 09:32:09 +0700

    build: add AVX2 SIMD headers

--- include/mandelbrot.h
+++ include/mandelbrot.h
@@ -3,6 +3,10 @@
 
 #include "config.h"
 
+#ifdef __AVX2__
+#include <immintrin.h>
+#endif
+
 typedef struct {
     double re;
     double im;
@@ -11,5 +15,10 @@ typedef struct {
 // get iteration count for mandelbrot point
 // returns MAX_ITERATIONS if it's in the set
 double mandelbrot_check(complex_t c);
+
+#ifdef __AVX2__
+// SIMD version: process 4 pixels at once
+void mandelbrot_check_avx2(const double *re, const double *im, double *results);
+#endif
 
 #endif
