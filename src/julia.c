6add3d78f3213d7ef0c4b88fc216c962ed9a4227
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-04-14 19:29:52 +0700

    feat(wasm): implement SIMD-accelerated smooth coloring

--- src/julia.c
+++ src/julia.c
@@ -1,12 +1,58 @@
 #include "julia.h"
 #include <math.h>
 
+#ifdef __wasm_simd128__
+#include <wasm_simd128.h>
+void julia_check_wasm_simd128(const double *re, const double *im, complex_t c, int max_iterations, double *results) {
+    v128_t cre = wasm_f64x2_splat(c.re);
+    v128_t cim = wasm_f64x2_splat(c.im);
+    v128_t zre = wasm_v128_load(re);
+    v128_t zim = wasm_v128_load(im);
+
+    v128_t iters = wasm_f64x2_splat(0.0);
+    v128_t esc_radius_sq = wasm_f64x2_splat(ESCAPE_RADIUS * ESCAPE_RADIUS);
+    v128_t escaped_mask = wasm_i64x2_make(0, 0);
+    v128_t final_mag_sq = wasm_f64x2_splat(0.0);
+
+    for (int i = 0; i < max_iterations; i++) {
+        if (wasm_i64x2_all_true(wasm_v128_and(escaped_mask, wasm_i64x2_make(0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF)))) break;
+
+        v128_t zre2 = wasm_f64x2_mul(zre, zre);
+        v128_t zim2 = wasm_f64x2_mul(zim, zim);
+        v128_t next_re = wasm_f64x2_add(wasm_f64x2_sub(zre2, zim2), cre);
+        v128_t next_im = wasm_f64x2_add(wasm_f64x2_mul(wasm_f64x2_splat(2.0), wasm_f64x2_mul(zre, zim)), cim);
+        zre = next_re;
+        zim = next_im;
+
+        v128_t mag_sq = wasm_f64x2_add(wasm_f64x2_mul(zre, zre), wasm_f64x2_mul(zim, zim));
+        v128_t mask = wasm_f64x2_gt(mag_sq, esc_radius_sq);
+        v128_t just_escaped = wasm_v128_andnot(mask, escaped_mask);
+        
+        final_mag_sq = wasm_v128_or(final_mag_sq, wasm_v128_and(just_escaped, mag_sq));
+        escaped_mask = wasm_v128_or(escaped_mask, mask);
+        iters = wasm_f64x2_add(iters, wasm_v128_andnot(wasm_f64x2_splat(1.0), escaped_mask));
+    }
+
+    double res_iters[2], res_mag_sq[2];
+    wasm_v128_store(res_iters, iters);
+    wasm_v128_store(res_mag_sq, final_mag_sq);
+
+    for (int i = 0; i < 2; i++) {
+        if (res_iters[i] >= max_iterations - 1) {
+            results[i] = (double)max_iterations;
+        } else {
+            results[i] = res_iters[i] + 2.0 - log2(log(res_mag_sq[i]));
+        }
+    }
+}
+#endif
+
 double julia_check(complex_t z, complex_t c, int max_iterations) {
     int iterations = 0;
     const double escape_radius_sq = ESCAPE_RADIUS * ESCAPE_RADIUS;
 
     while (iterations < max_iterations) {
-        // calculate next z
+        /* next z */
         double next_re = z.re * z.re - z.im * z.im + c.re;
         double next_im = 2 * z.re * z.im + c.im;
         z.re = next_re;
@@ -14,7 +60,7 @@ double julia_check(complex_t z, complex_t c, int max_iterations) {
 
         double mag_sq = z.re * z.re + z.im * z.im;
         if (mag_sq > escape_radius_sq) {
-            // calculate smooth color
+            /* smooth color */
             return (double)iterations + 2.0 - log2(log(mag_sq));
         }
 
--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -4,7 +4,7 @@
 #include <immintrin.h>
 
 double mandelbrot_check(complex_t c, int max_iterations) {
-    // check if point is in main cardioid
+    /* cardioid check */
     double cr_minus_025 = c.re - 0.25;
     double im_sq = c.im * c.im;
     double q = cr_minus_025 * cr_minus_025 + im_sq;
@@ -12,7 +12,7 @@ double mandelbrot_check(complex_t c, int max_iterations) {
         return (double)max_iterations;
     }
 
-    // check if point is in period-2 bulb
+    /* period-2 bulb check */
     double cr_plus_1 = c.re + 1.0;
     if (cr_plus_1 * cr_plus_1 + im_sq <= 0.0625) {
         return (double)max_iterations;
@@ -24,7 +24,7 @@ double mandelbrot_check(complex_t c, int max_iterations) {
     const double escape_radius_sq = ESCAPE_RADIUS * ESCAPE_RADIUS;
 
     while (iterations < max_iterations) {
-        // calculate next z
+        /* next z */
         double next_re = z.re * z.re - z.im * z.im + c.re;
         double next_im = 2 * z.re * z.im + c.im;
         z.re = next_re;
@@ -32,7 +32,7 @@ double mandelbrot_check(complex_t c, int max_iterations) {
 
         double mag_sq = z.re * z.re + z.im * z.im;
         if (mag_sq > escape_radius_sq) {
-            // calculate smooth color
+            /* smooth color */
             return (double)iterations + 2.0 - log2(log(mag_sq));
         }
 
@@ -47,7 +47,7 @@ void mandelbrot_check_avx2(const double *re, const double *im, int max_iteration
     __m256d cre = _mm256_loadu_pd(re);
     __m256d cim = _mm256_loadu_pd(im);
 
-    // Quick cardioid/bulb check
+    /* cardioid/bulb check */
     __m256d cre_m_025 = _mm256_sub_pd(cre, _mm256_set1_pd(0.25));
     __m256d cim2 = _mm256_mul_pd(cim, cim);
     __m256d q = _mm256_add_pd(_mm256_mul_pd(cre_m_025, cre_m_025), cim2);
@@ -94,6 +94,59 @@ void mandelbrot_check_avx2(const double *re, const double *im, int max_iteration
 
     for (int i = 0; i < 4; i++) {
         if (res_in_set[i] || res_iters[i] >= max_iterations - 1) {
+            results[i] = (double)max_iterations;
+        } else {
+            results[i] = res_iters[i] + 2.0 - log2(log(res_mag_sq[i]));
+        }
+    }
+}
+#endif
+
+#ifdef __wasm_simd128__
+#include <wasm_simd128.h>
+void mandelbrot_check_wasm_simd128(const double *re, const double *im, int max_iterations, double *results) {
+    v128_t cre = wasm_v128_load(re);
+    v128_t cim = wasm_v128_load(im);
+
+    /* cardioid check */
+    v128_t cre_m_025 = wasm_f64x2_sub(cre, wasm_f64x2_splat(0.25));
+    v128_t cim2 = wasm_f64x2_mul(cim, cim);
+    v128_t q = wasm_f64x2_add(wasm_f64x2_mul(cre_m_025, cre_m_025), cim2);
+    v128_t cardioid_mask = wasm_f64x2_le(wasm_f64x2_mul(q, wasm_f64x2_add(q, cre_m_025)), 
+                                        wasm_f64x2_mul(wasm_f64x2_splat(0.25), cim2));
+
+    v128_t zre = wasm_f64x2_splat(0.0);
+    v128_t zim = wasm_f64x2_splat(0.0);
+    v128_t iters = wasm_f64x2_splat(0.0);
+    v128_t esc_radius_sq = wasm_f64x2_splat(ESCAPE_RADIUS * ESCAPE_RADIUS);
+    v128_t escaped_mask = cardioid_mask;
+    v128_t final_mag_sq = wasm_f64x2_splat(0.0);
+
+    for (int i = 0; i < max_iterations; i++) {
+        if (wasm_i64x2_all_true(wasm_v128_and(escaped_mask, wasm_i64x2_make(0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF)))) break;
+
+        v128_t zre2 = wasm_f64x2_mul(zre, zre);
+        v128_t zim2 = wasm_f64x2_mul(zim, zim);
+        v128_t next_re = wasm_f64x2_add(wasm_f64x2_sub(zre2, zim2), cre);
+        v128_t next_im = wasm_f64x2_add(wasm_f64x2_mul(wasm_f64x2_splat(2.0), wasm_f64x2_mul(zre, zim)), cim);
+        zre = next_re;
+        zim = next_im;
+
+        v128_t mag_sq = wasm_f64x2_add(wasm_f64x2_mul(zre, zre), wasm_f64x2_mul(zim, zim));
+        v128_t mask = wasm_f64x2_gt(mag_sq, esc_radius_sq);
+        v128_t just_escaped = wasm_v128_andnot(mask, escaped_mask);
+        
+        final_mag_sq = wasm_v128_or(final_mag_sq, wasm_v128_and(just_escaped, mag_sq));
+        escaped_mask = wasm_v128_or(escaped_mask, mask);
+        iters = wasm_f64x2_add(iters, wasm_v128_andnot(wasm_f64x2_splat(1.0), escaped_mask));
+    }
+
+    double res_iters[2], res_mag_sq[2];
+    wasm_v128_store(res_iters, iters);
+    wasm_v128_store(res_mag_sq, final_mag_sq);
+
+    for (int i = 0; i < 2; i++) {
+        if (res_iters[i] >= max_iterations - 1) {
             results[i] = (double)max_iterations;
         } else {
             results[i] = res_iters[i] + 2.0 - log2(log(res_mag_sq[i]));
