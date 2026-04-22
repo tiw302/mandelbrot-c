8ddb3a57074455a607a355355253cdcf02d5ee96
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-04-22 12:01:34 +0700

    feat(web): add coi-serviceworker.js for multi-threading support

---
+++ web/coi-serviceworker.js
@@ -0,0 +1,39 @@
+/*! coi-serviceworker v0.1.7 - github.com/gzuidhof/coi-serviceworker */
+if (typeof window === 'undefined') {
+    self.addEventListener("install", () => self.skipWaiting());
+    self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+    self.addEventListener("fetch", (event) => {
+        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+            return;
+        }
+        event.respondWith(
+            fetch(event.request).then((response) => {
+                if (response.status === 0) return response;
+                const newHeaders = new Headers(response.headers);
+                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+                return new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: newHeaders,
+                });
+            }).catch(e => {
+                console.error(e);
+            })
+        );
+    });
+} else {
+    const load = () => {
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker.register(window.location.pathname + "coi-serviceworker.js").then((registration) => {
+                registration.addEventListener("updatefound", () => {
+                    window.location.reload();
+                });
+                if (registration.active && !navigator.serviceWorker.controller) {
+                    window.location.reload();
+                }
+            });
+        }
+    };
+    load();
+}
