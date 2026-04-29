2aef1ab9b80caa27a8cc0ab698dfa8925cfbaaa7
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-04-29 11:28:29 +0700

    refactor: format comments to lowercase in color core

--- src/core/color.c
+++ src/core/color.c
@@ -30,7 +30,7 @@ void get_color(double iterations, int max_iterations, uint8_t* r, uint8_t* g, ui
     double r1, g1, b1, r2, g2, b2;
 
     switch (current_palette) {
-        case 0: /* Sine Wave: Use 4, 2, 0 for Mint */
+        case 0: /* sine wave: use 4, 2, 0 for mint */
             r1 = sin(0.1 * fi + 4.0) * 127.0 + 128.0;
             g1 = sin(0.1 * fi + 2.0) * 127.0 + 128.0;
             b1 = sin(0.1 * fi + 0.0) * 127.0 + 128.0;
@@ -38,23 +38,23 @@ void get_color(double iterations, int max_iterations, uint8_t* r, uint8_t* g, ui
             g2 = sin(0.1 * (fi + 1.0) + 2.0) * 127.0 + 128.0;
             b2 = sin(0.1 * (fi + 1.0) + 0.0) * 127.0 + 128.0;
             break;
-        case 1: /* Grayscale */
+        case 1: /* grayscale */
             r1 = g1 = b1 = fmod(fi, 256.0);
             r2 = g2 = b2 = fmod(fi + 1.0, 256.0);
             break;
-        case 2: /* Fire */
+        case 2: /* fire */
             r1 = fmin(255.0, fi * 1.0); g1 = fmin(255.0, fi * 2.0); b1 = fmin(255.0, fi * 4.0);
             r2 = fmin(255.0, (fi+1.0) * 1.0); g2 = fmin(255.0, (fi+1.0) * 2.0); b2 = fmin(255.0, (fi+1.0) * 4.0);
             break;
-        case 3: /* Electric */
+        case 3: /* electric */
             r1 = fmin(255.0, fi * 8.0); g1 = fmin(255.0, fi * 4.0); b1 = fmin(255.0, fi * 1.0);
             r2 = fmin(255.0, (fi+1.0) * 8.0); g2 = fmin(255.0, (fi+1.0) * 4.0); b2 = fmin(255.0, (fi+1.0) * 1.0);
             break;
-        case 4: /* Ocean */
+        case 4: /* ocean */
             r1 = fmin(255.0, fi * 5.0); g1 = fmin(255.0, fi * 2.0); b1 = fmin(255.0, fi * 0.5);
             r2 = fmin(255.0, (fi+1.0) * 5.0); g2 = fmin(255.0, (fi+1.0) * 2.0); b2 = fmin(255.0, (fi+1.0) * 0.5);
             break;
-        case 5: /* Inferno */
+        case 5: /* inferno */
             r1 = fmin(255.0, fi * 0.5); g1 = fmin(255.0, fi * 2.0); b1 = fmin(255.0, fi * 8.0);
             r2 = fmin(255.0, (fi+1.0) * 0.5); g2 = fmin(255.0, (fi+1.0) * 2.0); b2 = fmin(255.0, (fi+1.0) * 8.0);
             break;
