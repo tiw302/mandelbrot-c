452b9d5099ddaa58962b3301e8914b262ecfe13e
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-26 12:25:09 +0700

    perf: optimize 128-bit SIMD implementation for Burning Ship

--- src/core/burning_ship.c
+++ src/core/burning_ship.c
@@ -180,16 +180,12 @@ double burning_ship_check_f128(simd_f128 cre, simd_f128 cim, int max_iterations)
     simd_f128 zim = simd_f128_from_double(0.0);
     int iterations = 0;
     const double escape_radius_sq = ESCAPE_RADIUS * ESCAPE_RADIUS;
-    
-    simd_f128 two = simd_f128_from_double(2.0);
 
     while (iterations < max_iterations) {
-        simd_f128 zre2 = simd_f128_mul(zre, zre);
-        simd_f128 zim2 = simd_f128_mul(zim, zim);
-        simd_f128 mag_sq_128 = simd_f128_add(zre2, zim2);
+        simd_f128 zre2 = simd_f128_sqr(zre);
+        simd_f128 zim2 = simd_f128_sqr(zim);
         
-        double mag_hi, mag_lo;
-        simd_f128_extract(mag_sq_128, &mag_hi, &mag_lo);
+        double mag_hi = simd_f128_get_hi(zre2) + simd_f128_get_hi(zim2);
         
         if (mag_hi > escape_radius_sq) {
             return (double)iterations + 2.0 - log2(log(fmax(1.0, mag_hi)));
@@ -199,7 +195,7 @@ double burning_ship_check_f128(simd_f128 cre, simd_f128 cim, int max_iterations)
         simd_f128 abs_im = f128_abs(zim);
         
         simd_f128 zre_zim = simd_f128_mul(abs_re, abs_im);
-        zim = simd_f128_add(simd_f128_mul(two, zre_zim), cim);
+        zim = simd_f128_add(simd_f128_mul2(zre_zim), cim);
         zre = simd_f128_add(simd_f128_sub(zre2, zim2), cre);
         
         iterations++;
