f0517bf85b9f7ffb556a55694fffc08e6602af94
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-28 22:31:02 +0700

    refactor(hud_sdl): move header to include/ and update signature

--- include/hud_sdl.h
+++ include/hud_sdl.h
@@ -5,7 +5,7 @@
 #include <SDL2/SDL_ttf.h>
 #include "app_state.h"
 
-void hud_render_sdl(SDL_Renderer* renderer, TTF_Font* font, const AppCommonState* state,
+void hud_render_sdl(SDL_Renderer* renderer, TTF_Font* font, AppCommonState* state,
                     int win_w, int win_h, int cpu_precision_128, uint32_t now);
 
 #endif
