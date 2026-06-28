f5105f13f60914c2c0eba556a3be76b076642a48
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-28 22:31:40 +0700

    feat(buffalo): add header declarations for Buffalo fractal kernels

---
+++ include/buffalo.h
@@ -0,0 +1,29 @@
+#ifndef BUFFALO_H
+#define BUFFALO_H
+
+#include "core_math.h"
+
+// standard 64-bit precision kernels
+double buffalo_check(complex_t c, int max_iterations);
+#ifdef __AVX2__
+void buffalo_check_avx2(__m256d cre, __m256d cim, int max_iterations, double* results);
+#endif
+#ifdef __AVX512F__
+void buffalo_check_avx512(__m512d cre, __m512d cim, int max_iterations, double* results);
+#endif
+#ifdef __wasm_simd128__
+void buffalo_check_wasm_simd128(v128_t cre, v128_t cim, int max_iterations, double* results);
+#endif
+#ifdef __ARM_NEON
+void buffalo_check_neon(float64x2_t cre, float64x2_t cim, int max_iterations, double* results);
+#endif
+
+// high-precision 128-bit kernels
+#ifdef USE_SIMD_F128
+double buffalo_check_f128(simd_f128 cre, simd_f128 cim, int max_iterations);
+#ifdef __AVX2__
+void buffalo_check_f128x4(simd_f128x4 cre, simd_f128x4 cim, int max_iterations, double* results);
+#endif
+#endif
+
+#endif
