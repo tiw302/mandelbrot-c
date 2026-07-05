ff04c62512405ddbb343302cb3b40458455ecf2c
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-05 23:02:12 +0700

    refactor(imgui_impl): update imgui_impl.cc for unified architecture
    - Refactor imgui_impl.cc to support the new unified app_runner and sokol backend

---
+++ src/ui/imgui_impl.cc
@@ -0,0 +1,16 @@
+#if defined(__EMSCRIPTEN__)
+#define SOKOL_GLES3
+#else
+#define SOKOL_GLCORE
+#endif
+#include "sokol/sokol_gfx.h"
+#include "sokol/sokol_app.h"
+
+#ifndef SG_PIXELFORMAT_SBGR8A8
+#define SG_PIXELFORMAT_SBGR8A8 44
+#endif
+
+#include "imgui.h"
+
+#define SOKOL_IMGUI_IMPL
+#include "sokol/sokol_imgui.h"
