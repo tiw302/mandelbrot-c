87db5944b4c7b5e3073c1aac9d61997f2cdab46f
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-16 09:34:55 +0700

    feat(ui): add video studio HUD declarations

---
+++ include/ui/hud_video_studio.h
@@ -0,0 +1,10 @@
+#ifndef HUD_VIDEO_STUDIO_H
+#define HUD_VIDEO_STUDIO_H
+
+#include "app_state.h"
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "cimgui.h"
+
+void hud_render_video_studio(struct ImFont* custom_font, AppCommonState* state, int win_w, int win_h, int* cpu_precision_128, uint32_t now);
+
+#endif
