915c7f98f1eeb8fd8b6b3540e61596934d2efc19
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-18 22:38:08 +0700

    chore(third_party/simd-f128): update simd_f128_vector.h from https://github.com/tiw302/simd-f128

--- third_party/simd-f128/simd_f128_vector.h
+++ third_party/simd-f128/simd_f128_vector.h
@@ -64,6 +64,8 @@ SIMD_F128_INLINE simd_f128x4 simd_f128x4_add(simd_f128x4 a, simd_f128x4 b) {
 // perform parallel multiplication of four double-double values
 SIMD_F128_INLINE simd_f128x4 simd_f128x4_mul(simd_f128x4 a, simd_f128x4 b) {
     __m256d hi_prod = _mm256_mul_pd(a.hi, b.hi);
+    __m256d diff = _mm256_sub_pd(hi_prod, hi_prod);
+    __m256d inf_mask = _mm256_cmp_pd(diff, diff, _CMP_UNORD_Q);
     
     // exact multiplication error estimation:
     // uses hardware fma if supported, otherwise falls back to a vector split method
@@ -87,6 +89,9 @@ SIMD_F128_INLINE simd_f128x4 simd_f128x4_mul(simd_f128x4 a, simd_f128x4 b) {
     simd_f128x4 res;
     res.hi = _mm256_add_pd(hi_prod, lo_prod);
     res.lo = _mm256_sub_pd(lo_prod, _mm256_sub_pd(res.hi, hi_prod));
+    // if hi_prod is infinite or nan, set lo component to zero and restore hi_prod to prevent nan propagation
+    res.hi = _mm256_blendv_pd(res.hi, hi_prod, inf_mask);
+    res.lo = _mm256_blendv_pd(res.lo, _mm256_setzero_pd(), inf_mask);
     return res;
 }
 
@@ -113,28 +118,7 @@ SIMD_F128_INLINE simd_f128x4 simd_f128x4_mul2(simd_f128x4 a) {
 
 // square all four double-double elements in parallel (a * a)
 SIMD_F128_INLINE simd_f128x4 simd_f128x4_sqr(simd_f128x4 a) {
-    __m256d hi_prod = _mm256_mul_pd(a.hi, a.hi);
-    
-    // exact multiplication error estimation for squaring
-#if defined(__FMA__)
-    __m256d err = _mm256_fmsub_pd(a.hi, a.hi, hi_prod);
-#else
-    // vector split using dekker's constant (2^27 + 1)
-    __m256d c = _mm256_set1_pd(134217729.0);
-    __m256d up = _mm256_mul_pd(a.hi, c);
-    __m256d u1 = _mm256_sub_pd(up, _mm256_sub_pd(up, a.hi));
-    __m256d u2 = _mm256_sub_pd(a.hi, u1);
-    __m256d err = _mm256_add_pd(_mm256_add_pd(_mm256_sub_pd(_mm256_mul_pd(u1, u1), hi_prod), _mm256_mul_pd(_mm256_add_pd(u1, u1), u2)), _mm256_mul_pd(u2, u2));
-#endif
-
-    // accumulate cross-terms (2.0 * a.hi * a.lo) and squaring roundoff error
-    __m256d cross = _mm256_mul_pd(a.hi, a.lo);
-    __m256d lo_prod = _mm256_add_pd(_mm256_add_pd(cross, cross), err);
-    
-    simd_f128x4 res;
-    res.hi = _mm256_add_pd(hi_prod, lo_prod);
-    res.lo = _mm256_sub_pd(lo_prod, _mm256_sub_pd(res.hi, hi_prod));
-    return res;
+    return simd_f128x4_mul(a, a);
 }
 
 // compute absolute values of all four double-double elements in parallel
@@ -153,44 +137,112 @@ SIMD_F128_INLINE simd_f128x4 simd_f128x4_abs(simd_f128x4 a) {
     return res;
 }
 
-// perform parallel division of four double-double elements lane-by-lane
+// perform parallel division of four double-double elements using avx2 vector math
 SIMD_F128_INLINE simd_f128x4 simd_f128x4_div(simd_f128x4 a, simd_f128x4 b) {
-    double ahi[4], alo[4], bhi[4], blo[4];
-    _mm256_storeu_pd(ahi, a.hi);
-    _mm256_storeu_pd(alo, a.lo);
-    _mm256_storeu_pd(bhi, b.hi);
-    _mm256_storeu_pd(blo, b.lo);
+    __m256d q1 = _mm256_div_pd(a.hi, b.hi);
+    __m256d p1 = _mm256_mul_pd(q1, b.hi);
     
-    double rhi[4], rlo[4];
-    for (int i = 0; i < 4; i++) {
-        simd_f128 sa = simd_f128_from_hi_lo(ahi[i], alo[i]);
-        simd_f128 sb = simd_f128_from_hi_lo(bhi[i], blo[i]);
-        simd_f128 sr = simd_f128_div(sa, sb);
-        simd_f128_extract(sr, &rhi[i], &rlo[i]);
-    }
-    
+#if defined(__FMA__)
+    __m256d p2 = _mm256_fmadd_pd(q1, b.lo, _mm256_fmsub_pd(q1, b.hi, p1));
+#else
+    // dekker's split fallback for vector FMA if __FMA__ is not defined
+    __m256d c = _mm256_set1_pd(134217729.0);
+    __m256d up = _mm256_mul_pd(q1, c);
+    __m256d vp = _mm256_mul_pd(b.hi, c);
+    __m256d u1 = _mm256_sub_pd(up, _mm256_sub_pd(up, q1));
+    __m256d u2 = _mm256_sub_pd(q1, u1);
+    __m256d v1 = _mm256_sub_pd(vp, _mm256_sub_pd(vp, b.hi));
+    __m256d v2 = _mm256_sub_pd(b.hi, v1);
+    __m256d mul_err = _mm256_add_pd(_mm256_add_pd(_mm256_add_pd(_mm256_sub_pd(_mm256_mul_pd(u1, v1), p1), _mm256_mul_pd(u1, v2)), _mm256_mul_pd(u2, v1)), _mm256_mul_pd(u2, v2));
+    __m256d p2 = _mm256_add_pd(_mm256_mul_pd(q1, b.lo), mul_err);
+#endif
+
+    __m256d s = _mm256_sub_pd(a.hi, p1);
+    __m256d v = _mm256_sub_pd(s, a.hi);
+    __m256d e = _mm256_add_pd(
+        _mm256_sub_pd(a.hi, _mm256_sub_pd(s, v)),
+        _mm256_sub_pd(_mm256_sub_pd(_mm256_setzero_pd(), p1), v)
+    );
+    __m256d t = _mm256_add_pd(_mm256_sub_pd(a.lo, p2), e);
+
+    __m256d rh = _mm256_add_pd(s, t);
+    __m256d rl = _mm256_sub_pd(t, _mm256_sub_pd(rh, s));
+    __m256d q2 = _mm256_div_pd(rh, b.hi);
+
     simd_f128x4 res;
-    res.hi = _mm256_loadu_pd(rhi);
-    res.lo = _mm256_loadu_pd(rlo);
+    res.hi = _mm256_add_pd(q1, q2);
+    res.lo = _mm256_add_pd(_mm256_sub_pd(q2, _mm256_sub_pd(res.hi, q1)), _mm256_div_pd(rl, b.hi));
+
+    // check if q1 is Inf/NaN
+    __m256d q1_invalid = _mm256_cmp_pd(_mm256_sub_pd(q1, q1), _mm256_setzero_pd(), _CMP_UNORD_Q);
+    res.hi = _mm256_blendv_pd(res.hi, q1, q1_invalid);
+    res.lo = _mm256_blendv_pd(res.lo, _mm256_setzero_pd(), q1_invalid);
+
+    // prevent nan/inf propagation by zeroing lo component on overflow/underflow
+    __m256d diff = _mm256_sub_pd(res.hi, res.hi);
+    __m256d inf_mask = _mm256_cmp_pd(diff, diff, _CMP_UNORD_Q);
+    res.lo = _mm256_blendv_pd(res.lo, _mm256_setzero_pd(), inf_mask);
+
     return res;
 }
 
-// perform parallel square root of four double-double elements lane-by-lane
+// perform parallel square root of four double-double elements using avx2 vector math
 SIMD_F128_INLINE simd_f128x4 simd_f128x4_sqrt(simd_f128x4 a) {
-    double ahi[4], alo[4];
-    _mm256_storeu_pd(ahi, a.hi);
-    _mm256_storeu_pd(alo, a.lo);
+    __m256d zero = _mm256_setzero_pd();
+    __m256d one = _mm256_set1_pd(1.0);
     
-    double rhi[4], rlo[4];
-    for (int i = 0; i < 4; i++) {
-        simd_f128 sa = simd_f128_from_hi_lo(ahi[i], alo[i]);
-        simd_f128 sr = simd_f128_sqrt(sa);
-        simd_f128_extract(sr, &rhi[i], &rlo[i]);
-    }
+    // initial hardware guess for 1/sqrt(xhi)
+    __m256d y = _mm256_div_pd(one, _mm256_sqrt_pd(a.hi));
+    __m256d z = _mm256_mul_pd(a.hi, y);
+
+#if defined(__FMA__)
+    __m256d zlo = _mm256_fmadd_pd(a.lo, y, _mm256_fmsub_pd(a.hi, y, z));
+#else
+    __m256d c = _mm256_set1_pd(134217729.0);
+    __m256d up = _mm256_mul_pd(a.hi, c);
+    __m256d vp = _mm256_mul_pd(y, c);
+    __m256d u1 = _mm256_sub_pd(up, _mm256_sub_pd(up, a.hi));
+    __m256d u2 = _mm256_sub_pd(a.hi, u1);
+    __m256d v1 = _mm256_sub_pd(vp, _mm256_sub_pd(vp, y));
+    __m256d v2 = _mm256_sub_pd(y, v1);
+    __m256d mul_err = _mm256_add_pd(_mm256_add_pd(_mm256_add_pd(_mm256_sub_pd(_mm256_mul_pd(u1, v1), z), _mm256_mul_pd(u1, v2)), _mm256_mul_pd(u2, v1)), _mm256_mul_pd(u2, v2));
+    __m256d zlo = _mm256_add_pd(_mm256_mul_pd(a.lo, y), mul_err);
+#endif
+
+    __m256d est = _mm256_mul_pd(z, z);
+
+#if defined(__FMA__)
+    __m256d estlo = _mm256_fmadd_pd(_mm256_mul_pd(_mm256_set1_pd(2.0), z), zlo, _mm256_fmsub_pd(z, z, est));
+#else
+    __m256d up2 = _mm256_mul_pd(z, c);
+    __m256d u1_2 = _mm256_sub_pd(up2, _mm256_sub_pd(up2, z));
+    __m256d u2_2 = _mm256_sub_pd(z, u1_2);
+    __m256d mul_err2 = _mm256_add_pd(_mm256_add_pd(_mm256_add_pd(_mm256_sub_pd(_mm256_mul_pd(u1_2, u1_2), est), _mm256_mul_pd(u1_2, u2_2)), _mm256_mul_pd(u2_2, u1_2)), _mm256_mul_pd(u2_2, u2_2));
+    __m256d estlo = _mm256_add_pd(_mm256_mul_pd(_mm256_mul_pd(_mm256_set1_pd(2.0), z), zlo), mul_err2);
+#endif
+
+    // refine initial guess and compute error of z^2 vs x
+    __m256d err = _mm256_add_pd(_mm256_sub_pd(_mm256_sub_pd(a.hi, est), estlo), a.lo);
+    __m256d half_err_y = _mm256_mul_pd(_mm256_set1_pd(0.5), _mm256_mul_pd(err, y));
+
+    __m256d final_hi = _mm256_add_pd(z, half_err_y);
+    __m256d final_lo = _mm256_sub_pd(half_err_y, _mm256_sub_pd(final_hi, z));
+
+    // handle bounds and special values
+    __m256d lt_zero = _mm256_cmp_pd(a.hi, zero, _CMP_LT_OS);
+    __m256d eq_zero = _mm256_cmp_pd(a.hi, zero, _CMP_EQ_OQ);
+    __m256d lo_lt_zero = _mm256_cmp_pd(a.lo, zero, _CMP_LT_OS);
+    __m256d invalid_mask = _mm256_or_pd(lt_zero, _mm256_and_pd(eq_zero, lo_lt_zero));
     
+    __m256d is_inf = _mm256_cmp_pd(a.hi, _mm256_set1_pd(INFINITY), _CMP_EQ_OQ);
+    __m256d pass_mask = _mm256_or_pd(eq_zero, is_inf);
+
     simd_f128x4 res;
-    res.hi = _mm256_loadu_pd(rhi);
-    res.lo = _mm256_loadu_pd(rlo);
+    res.hi = _mm256_blendv_pd(final_hi, a.hi, pass_mask);
+    res.lo = _mm256_blendv_pd(final_lo, a.lo, pass_mask);
+    res.hi = _mm256_blendv_pd(res.hi, _mm256_set1_pd(NAN), invalid_mask);
+    res.lo = _mm256_blendv_pd(res.lo, zero, invalid_mask);
+
     return res;
 }
 
@@ -255,7 +307,7 @@ SIMD_F128_INLINE simd_f128x4 simd_f128x4_mul2(simd_f128x4 a) {
 SIMD_F128_INLINE simd_f128x4 simd_f128x4_sqr(simd_f128x4 a) {
     simd_f128x4 res;
     for (int i = 0; i < 4; i++) {
-        res.val[i] = simd_f128_sqr(a.val[i]);
+        res.val[i] = simd_f128_mul(a.val[i], a.val[i]);
     }
     return res;
 }
