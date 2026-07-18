fb282ada21776222731985f0a74ec15c372b956d
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-18 22:38:08 +0700

    feat(ui/hud_settings): update/add hud_settings header

--- include/ui/hud_settings.h
+++ include/ui/hud_settings.h
@@ -1,3 +1,7 @@
+/* hud_settings.h
+ *
+ * settings panel ui definitions.
+ */
 #ifndef HUD_SETTINGS_H
 #define HUD_SETTINGS_H
 
@@ -5,6 +9,7 @@
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include "cimgui.h"
 
-void hud_render_settings_window(AppCommonState* state, int win_w, int win_h, int gpu_mode, int* high_precision_mode, int* use_perturbation);
+void hud_render_settings_window(AppCommonState* state, int win_w, int win_h, int gpu_mode,
+                                int* high_precision_mode, int* use_perturbation);
 
-#endif // HUD_SETTINGS_H
+#endif  // HUD_SETTINGS_H
