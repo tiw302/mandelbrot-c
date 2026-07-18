08412edae66791ee966f99240f254b137df91e67
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-18 22:38:08 +0700

    feat(web): update web frontend web_main.c

--- src/app/web_main.c
+++ src/app/web_main.c
@@ -1,5 +1,5 @@
-/* 
- * [ARCH] platform entry point: web (webassembly / webgl)
+/* web_main.c
+ * platform entry point: web (webassembly / webgl)
  * 
  * we use separate entry point files for different build targets (cpu, gpu, web)
  * instead of massive #ifdef blocks. this keeps the cmake build targets clean
@@ -10,5 +10,7 @@
 #include "app_runner.h"
 
 sapp_desc sokol_main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
     return app_runner_get_desc(APP_BACKEND_WEB);
 }
