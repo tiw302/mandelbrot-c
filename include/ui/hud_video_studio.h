4705c2e380fbcf27ea9f7bee0fee74d0eea317dc
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-18 22:38:08 +0700

    feat(ui/hud_video_studio): update/add hud_video_studio header

--- include/ui/hud_video_studio.h
+++ include/ui/hud_video_studio.h
@@ -1,3 +1,7 @@
+/* hud_video_studio.h
+ *
+ * video export studio ui definitions.
+ */
 #ifndef HUD_VIDEO_STUDIO_H
 #define HUD_VIDEO_STUDIO_H
 
@@ -5,6 +9,7 @@
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include "cimgui.h"
 
-void hud_render_video_studio(struct ImFont* custom_font, AppCommonState* state, int win_w, int win_h, int* cpu_precision_128, uint32_t now);
+void hud_render_video_studio(struct ImFont* custom_font, AppCommonState* state, int win_w,
+                             int win_h, int* cpu_precision_128, uint32_t now);
 
 #endif
