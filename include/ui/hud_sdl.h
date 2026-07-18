ab206e4799e310d629a9564af15d9f22e646c7ea
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-18 22:38:08 +0700

    feat(ui/hud_sdl): update/add hud_sdl header

--- include/ui/hud_sdl.h
+++ include/ui/hud_sdl.h
@@ -1,11 +1,16 @@
+/* hud_sdl.h
+ *
+ * immediate-mode ui overlay for sdl backend.
+ */
 #ifndef HUD_SDL_H
 #define HUD_SDL_H
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+
 #include "app_state.h"
 
-void hud_render_sdl(SDL_Renderer* renderer, TTF_Font* font, AppCommonState* state,
-                    int win_w, int win_h, int cpu_precision_128, uint32_t now);
+void hud_render_sdl(SDL_Renderer* renderer, TTF_Font* font, AppCommonState* state, int win_w,
+                    int win_h, int cpu_precision_128, uint32_t now);
 
 #endif
