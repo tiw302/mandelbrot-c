c19a43451864212e80d4379e600552fe7ab75d7f
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-18 22:38:08 +0700

    feat(app): update app runner component headless_egl.c

--- src/app/headless_egl.c
+++ src/app/headless_egl.c
@@ -5,6 +5,7 @@
  */
 
 #include "headless_egl.h"
+
 #include <stdio.h>
 
 #ifdef __linux__
@@ -17,27 +18,45 @@ int init_headless_egl(int width, int height) {
         fprintf(stderr, "error: egl initialization failed\n");
         return 0;
     }
-    
+
     // request opengl api
     eglBindAPI(EGL_OPENGL_API);
-    
+
     // configure rgb8 surface with depth 8 (offscreen pbuffer)
-    EGLint config_attribs[] = { EGL_SURFACE_TYPE, EGL_PBUFFER_BIT, EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_DEPTH_SIZE, 8, EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT, EGL_NONE };
-    EGLConfig egl_cfg; 
+    EGLint config_attribs[] = {EGL_SURFACE_TYPE,
+                               EGL_PBUFFER_BIT,
+                               EGL_BLUE_SIZE,
+                               8,
+                               EGL_GREEN_SIZE,
+                               8,
+                               EGL_RED_SIZE,
+                               8,
+                               EGL_DEPTH_SIZE,
+                               8,
+                               EGL_RENDERABLE_TYPE,
+                               EGL_OPENGL_BIT,
+                               EGL_NONE};
+    EGLConfig egl_cfg;
     EGLint num_configs;
     if (!eglChooseConfig(egl_dpy, config_attribs, &egl_cfg, 1, &num_configs)) return 0;
-    
+
     // create the offscreen surface
-    EGLint pbuffer_attribs[] = { EGL_WIDTH, width, EGL_HEIGHT, height, EGL_NONE };
+    EGLint pbuffer_attribs[] = {EGL_WIDTH, width, EGL_HEIGHT, height, EGL_NONE};
     EGLSurface egl_surf = eglCreatePbufferSurface(egl_dpy, egl_cfg, pbuffer_attribs);
-    
+
     // request opengl 3.3 core profile
-    EGLint ctx_attribs[] = { EGL_CONTEXT_MAJOR_VERSION, 3, EGL_CONTEXT_MINOR_VERSION, 3, EGL_CONTEXT_OPENGL_PROFILE_MASK, EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT, EGL_NONE };
+    EGLint ctx_attribs[] = {EGL_CONTEXT_MAJOR_VERSION,
+                            3,
+                            EGL_CONTEXT_MINOR_VERSION,
+                            3,
+                            EGL_CONTEXT_OPENGL_PROFILE_MASK,
+                            EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT,
+                            EGL_NONE};
     EGLContext egl_ctx = eglCreateContext(egl_dpy, egl_cfg, EGL_NO_CONTEXT, ctx_attribs);
-    
+
     // make the context current
     if (!eglMakeCurrent(egl_dpy, egl_surf, egl_surf, egl_ctx)) return 0;
-    
+
     return 1;
 }
 #endif
